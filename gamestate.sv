d3260b0c38636036e48ce211c5ae823b04f317c9
JoonChicken <jbsquirrel06@gmail.com>
2025-11-23 21:03:51 -0500

    state machine complete?

--- gamestate.sv
+++ gamestate.sv
@@ -6,29 +6,43 @@ module gamestate (
     input logic reached_end,
     output logic [1:0] state,
     output logic [3:0] level,
-    output logic [2:0] sound_select
+    output logic [1:0] soundselector,
+    output logic playsound    // playsound HIGH -> play the sound specified
 );
     enum logic [1:0] {MENU, PLAYING, DEAD, WIN} statetype;
+    enum logic [1:0] {UI_PRESS, NEXTLEVEL, CRASH, CELEBRATION} soundtype;
 
     always_ff @(posedge clk) begin
         if (reset) begin
             state <= MENU;
             level <= 4'b0;
+            soundselector <= UI_PRESS;
+            playsound <= 1'b1;
         end else if (state == MENU && start) begin
             state <= PLAYING; 
+            soundselector <= UI_PRESS;
+            playsound <= 1'b1;
         end else if (state == PLAYING) begin
             if (reached_end) begin
                 if (level == 4'd15) begin
                     state <= WIN;
+                    soundselector <= CELEBRATION;
+                    playsound <= 1'b1;
                 end else begin
                     level <= level + 1;
+                    soundselector <= NEXTLEVEL;
+                    playsound <= 1'b1;
                 end
             end
             if (collision) begin
                 state <= DEAD;
                 level <= 4'b0;
+                soundselector <= CRASH;
+                playsound <= 1'b1;
             end
         end
+
+        playsound <= 1'b0;
     end
 
 endmodule
