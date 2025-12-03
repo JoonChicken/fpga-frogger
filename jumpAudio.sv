9501240cf3261f81a16218edb92b2a1360ca8532
ethanwienkamp-byte <ethan.wienkamp@tufts.edu>
2025-12-02 21:34:02 -0500

    Update jumpAudio.sv

--- jumpAudio.sv
+++ jumpAudio.sv
@@ -1,22 +1,25 @@
 //Divide 48MGHz into usable hertz for sound, play notes (boing? somehow)
 
-
-
-
-
 module jumpAudio (
 input logic clk,
 output logic jumpSoundOut
 );
 
+logic [16:0] curCount;
 
 
+always_ff @(posedge clk) begin
+curCount <=curCount + 1;
+end
 
 
-
-
-
-
-
+always_comb
+	if (curCount == 54545)
+		jumpSoundOut = 1’b1;
+		curCount = 0;
+	end else
+		jumpSoundOut = 1’b0;
+end
 
 endmodule 
+
