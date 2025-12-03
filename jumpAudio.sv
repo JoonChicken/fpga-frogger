8ef59b55ac765d6fa9b07c7b0e1e291f84749b2f
ethanwienkamp-byte <ethan.wienkamp@tufts.edu>
2025-12-02 21:08:30 -0500

    Create jumpAudio.sv

---
+++ jumpAudio.sv
@@ -0,0 +1,22 @@
+//Divide 48MGHz into usable hertz for sound, play notes (boing? somehow)
+
+
+
+
+
+module jumpAudio (
+input logic clk,
+output logic jumpSoundOut
+);
+
+
+
+
+
+
+
+
+
+
+
+endmodule 
