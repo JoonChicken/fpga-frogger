0b32b42cbecf116a3adfcb7f910032952b8a08c1
lgirou01 <Lauren.Girouard@tufts.edu>
2025-12-04 14:23:50 -0500

    Update logs.sv

--- logs.sv
+++ logs.sv
@@ -1,10 +1,7 @@
 module logs(
   input logic clk, 
   input logic reset, 
-  input logic [9:0] log_x, // this is the incoming log position 
-  input logic enter_screen, // gets a new log 
-  input logic exit_screen, // removes a log 
-  output logic [9:0] log_x_new // updated log position 
+  output logic [9:0] log_x_new[0:MAX_LOGS-1] // updated log position array
 );
 
 
@@ -14,22 +11,48 @@ module logs(
   // mkae sure it's not going too fast 
 
 // constants for the logs
-parameter blocksize = 10'd32; 
-parameter log_width = 3*10'd32; 
-parameter log_speed = 10'd4; 
-parameter screen_width = 10*10'd32; 
+parameter blocksize = 10'd32;  // blocksize 
+parameter log_width = 3*10'd32; // each log is 3 blocks wide 
+parameter log_height = blocksize; // heigh of the log 
+parameter log_speed = 10'd4;   // how fast log is moving 
+parameter screen_width = 10*10'd32; // screen is 10 blocks wide 
+parameter screen_height = 10*10'd32;; // screen height 
+parameter MAX_LOGS = 1; // number of logs per lane 
+
+logic [18:0] counter; 
+logic enable; 
+logic [9:0] log_x[0:MAX_LOGS-1];  // 0 represents the last log, MAX_LOGS-1 is the first log 
 
 // logs are only moving to the right at the moment
 always_ff @(posedge clk)begin
-  if (reset) begin
-    log_x_new <= -log_x_width; // resets it back to the initial position when we reset the game
-end else begin // log movement
-  log_x_new <= log_x + log_speed; 
-  if (log_x_new >= screen_width)begin   // gets the log at the starting point when it leaves the screen
-    log_x_new <= -log_x_width; 
+  counter = counter + 1; 
+  if (enable == 1) begin
+    if (reset) begin
+      counter <= 0; // counter goes back to zero once we reset 
+      for (int i = 0; i < MAX_LOGS; i++) begin 
+        log_x[i] <= -log_width; 
+      end 
+    end else begin // log movement
+      for (int i = 0; i < MAX_LOGS; i++) begin
+        log_x[i] <= log_x[i] + log_speed; 
+        if (log_x[i] >= screen_width) begin 
+          // gets the log at the starting point when it leaves the screen
+          log_x[i] <= -log_width; 
+        end 
+      end 
+    end 
+  end
+end 
+
+always_comb begin
+  if (counter == 19'b0111111111111111111) begin 
+    enable = 1; 
+  end else begin 
+    enable = 0; 
   end 
+  log_x_new = log_x; 
 end 
-end 
+    
 end module
 
 
