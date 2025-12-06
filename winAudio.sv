4c04efececca38c3e5e694ed08f0faa134d68035
ethanwienkamp-byte <ethan.wienkamp@tufts.edu>
2025-12-06 18:52:34 -0500

    Update winAudio.sv

--- winAudio.sv
+++ winAudio.sv
@@ -8,22 +8,18 @@ logic prevEnable;
 logic [16:0] freqCount;
 logic [23:0] timer;
 
-//start timer loops below if enabled
-always_ff @(posedge clk) begin
-	prevEnable <= enable;
-end
-
-//rising edge detector
-always_ff @(posedge clk) begin
-	if (enable == 1'b1 & prevEnable == 1'b0) begin
-		timerEnable <= 1'b1;
-	end else if (timer == 24'd12500000)
-		timerEnable <= 0;
-end
 
     //play sound at certain frequency until timer reaches desired duration
 always_ff @(posedge clk) begin
-	if (timer < 24'd13125000 & timerEnable) begin
+	prevEnable <= enable;
+
+	if (enable == 1'b1 & prevEnable == 1'b0) begin
+			timerEnable <= 1'b1;
+	end else if (timer >= 24'd12500000) begin
+			timerEnable <= 0;
+	end
+	
+	if (timer < 24'd3125000 & timerEnable) begin
 		timer <= timer + 1;
         
         if (freqCount < 4474) begin
@@ -64,7 +60,7 @@ always_ff @(posedge clk) begin
 			winSoundOut <=0;
 		end
 
-	end else if (timer == 24'd12500000) begin
+	end else if (timer >= 24'd12500000) begin
 		timer <= 24'b0;
 	end
 end
