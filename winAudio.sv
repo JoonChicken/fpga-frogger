c1da1cca23e13705a58f3e4c2b8c317661fc3dc3
ethanwienkamp-byte <ethan.wienkamp@tufts.edu>
2025-12-06 18:33:34 -0500

    Update winAudio.sv

--- winAudio.sv
+++ winAudio.sv
@@ -28,37 +28,37 @@ always_ff @(posedge clk) begin
         
         if (freqCount < 4474) begin
 			freqCount <= freqCount + 1;
-			jumpSoundOut <= 1;
+			winSoundOut <= 1;
         end else if (freqCount < 8948) begin
 			freqCount <= freqCount + 1;
-			jumpSoundOut <= 0;
+			winSoundOut <= 0;
         end else if (freqCount == 8948) begin
 			freqCount <= 0;
-			jumpSoundOut <=0;
+			winSoundOut <=0;
 		end
         
     end else if (timer < 24'd6250000 & timerEnable) begin
         if (freqCount < 3986) begin
 			freqCount <= freqCount + 1;
-			jumpSoundOut <= 1;
+			winSoundOut <= 1;
         end else if (freqCount < 7972) begin
 			freqCount <= freqCount + 1;
-			jumpSoundOut <= 0;
+			winSoundOut <= 0;
         end else if (freqCount == 7972) begin
 			freqCount <= 0;
-			jumpSoundOut <=0;
+			winSoundOut <=0;
 		end
 	    
     end else if (timer < 24'd12500000 & timerEnable) begin
         if (freqCount < 3551) begin
 			freqCount <= freqCount + 1;
-			jumpSoundOut <= 1;
+			winSoundOut <= 1;
         end else if (freqCount < 7102) begin
 			freqCount <= freqCount + 1;
-			jumpSoundOut <= 0;
+			winSoundOut <= 0;
         end else if (freqCount == 7102) begin
 			freqCount <= 0;
-			jumpSoundOut <=0;
+			winSoundOut <=0;
 		end
 
 	end else if (timer == 24'd12500000) begin
