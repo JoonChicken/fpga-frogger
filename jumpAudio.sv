59b520fee199cb62ed1d25122179d5c76ce240e9
ethanwienkamp-byte <ethan.wienkamp@tufts.edu>
2025-12-07 19:17:23 -0500

    Update jumpAudio.sv

--- jumpAudio.sv
+++ jumpAudio.sv
@@ -5,29 +5,12 @@ input logic clk,
 input logic enable,
 output logic jumpSoundOut
 );
-
-	logic prevEnable;
 	logic [16:0] freqCount;
 	logic [23:0] timer;
 
-	logic timerEnable;
-
-//start timer loops below if enabled
-always_ff @(posedge clk) begin
-	prevEnable <= enable;
-end
-
-//rising edge detector
-always_ff @(posedge clk) begin
-	if (enable == 1'b1 & prevEnable == 1'b0) begin
-		timerEnable <= 1'b1;
-	end else if (timer == 24'd12500000)
-		timerEnable <= 0;
-end
-
 //play sound at certain frequency until timer reaches desired duration
 always_ff @(posedge clk) begin
-	if (timer < 24'd12500000 & timerEnable) begin
+	if (timer < 24'd12500000 & enable) begin
 		timer <= timer + 1;
 		if (freqCount < 14261) begin
 			freqCount <= freqCount + 1;
